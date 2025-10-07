e467ba400f21ebc0f24d3f53cbe01726e7196dcc
IkerLopez <a24ikelopgom@inspedralbes.cat>
2025-10-07 09:02:25 +0200

    listado de gatos y caricias

--- js/clicker.js
+++ js/clicker.js
@@ -1,11 +1,21 @@
-let contRigby = 0;
-var contenidorRigby = document.getElementById("rigby").addEventListener('click', function(){
-    contRigby ++
-    document.getElementById("contRigby").innerHTML = "tens " + contRigby + " rigbys";
-})
+let data = {
+    gatosActivos: 0,
+    gatos: [
+        {name: "rigby", image: "media/rigby.jpeg", nclicks: 0},
+        {name: "gumbus", image: "media/gumbus.jpeg", nclicks: 0},
+        {name: "purrito", image: "media/purrito.jpeg", nclicks: 0},
+        {name: "gatitofeo", image: "media/gatitofeo.jpeg", nclicks: 0},
+    ]
+};
+//LEEMOS EL JSON Y CREAMOS LA LISTA DE GATITOS
+let htmlString = "";
+for (let i = 0; i < data.gatos.length; i++){
+    htmlString += `<l1 id = "${i}">${data.gatos[i].name}</l1><br>`;
+}
 
-let contGumbus = 0;
-var contenidorGumbus = document.getElementById("gumbus").addEventListener('click', function(){
-    contGumbus ++
-    document.getElementById("contGumbus").innerHTML = "tens " + contGumbus + " gumbuses";
-})
+document.getElementById("catlist").innerHTML = htmlString;
+
+//HACEMOS QUE CADA GATITO REACCIONE A LOS CLICKS
+document.getElementById("catlist").addEventListener('click', function(e){
+    console.log("Has acariciado a " + e.target.id)
+});
